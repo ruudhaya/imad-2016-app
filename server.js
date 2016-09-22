0db90cf3b40c26a3129abb3dc1544240a6b2bbea
Udhay <udhayapsg@gmail.com>
2016-09-23 00:09:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.send ("Article One Requested, coming Soon..");
+    res.sendFile (path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 
